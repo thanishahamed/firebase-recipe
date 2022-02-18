8889f9d254e6e410154586b55d616106a486ce3e
Thanish <muktharthanish@gmail.com>
2022-02-18 10:21:03 +0530

    upated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import React from "react";
 import "./App.css";
+import LoginForm from "./components/LoginForm";
+import FireBaseAuthService from "./FirebaseAuthService";
 // eslint-disable-next-line no-unused-vars
 import firebase from "./FirebaseConfig";
 function App() {
+  const [user, setUser] = React.useState(null);
+
+  FireBaseAuthService.subscribeToAuthChanges(setUser);
+
   return (
     <div className="App">
       <div>
-        <h1>My web app is running!</h1>
+        <h1>My web app is rudnnsdfsdafing!</h1>
+        <LoginForm existingUser={user} />
       </div>
     </div>
   );
---
+++ src/components/LoginForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+import FireBaseAuthService from "../FirebaseAuthService";
+
+const LoginForm = ({ existingUser }) => {
+  return (
+    <div>
+      <h1>LoginForm </h1>
+    </div>
+  );
+};
+
+export default LoginForm;
